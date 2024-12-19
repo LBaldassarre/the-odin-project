7e8c0c96be133fb8f0a43edf1a458f4691995468
lab <lab@pop-os.fritz.box>
2024-12-19 13:38:36 +0100

    Add palet functionality to the Etch-a-Sketch project

--- Etch-a-Sketch/script.js
+++ Etch-a-Sketch/script.js
@@ -5,7 +5,7 @@ function createDrawBoard (colNumber, rowNumber, drawBoard) {
         colHTML = '<div class="db_col">\n';
         var rowCounter = 0;
         while (rowCounter < rowNumber) {
-            var rowHTML = ' <div class="db_row"></div>\n';
+            var rowHTML = ' <div class="db_cell"></div>\n';
             colHTML += rowHTML;
             rowCounter++;
         }
@@ -16,6 +16,26 @@ function createDrawBoard (colNumber, rowNumber, drawBoard) {
     drawBoard.innerHTML = boardHTML;
 }
 
-var drawBoard = document.querySelector('.draw_pane__board');
+var paintColor = "black";
 
+var drawBoard = document.querySelector('.draw_pane__board');
 createDrawBoard (16,16,drawBoard);
+
+var cells = document.querySelectorAll('.db_cell');
+var p_colors = document.querySelectorAll('.p_color'); 
+
+
+cells.forEach(cell => cell.addEventListener('click', () => {
+    // cell.style.backgroundColor = "black"
+    cell.className = paintColor + ' db_cell';
+    console.log(cell.className);
+})
+);
+
+p_colors.forEach(p_color => p_color.addEventListener('click', () => {
+    var class_name = p_color.attributes.class.nodeValue;
+    var color = class_name.slice(-7);
+    paintColor = color;
+})
+);
+
