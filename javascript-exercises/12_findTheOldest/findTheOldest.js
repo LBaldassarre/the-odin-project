74e8a9f3fd4d405f3b6a2eed294b715c156ae2a8
lab <lab@pop-os.fritz.box>
2024-12-21 19:49:25 +0100

    Finish 11_getTheTitles and 12_findTheOldest

--- javascript-exercises/12_findTheOldest/findTheOldest.js
+++ javascript-exercises/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,11 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    const oldest = people.sort(function(a, b) {
+        const lastPerson = a.yearOfBirth - (("yearOfDeath" in a) ? a.yearOfDeath : (new Date()).getFullYear());
+        const nextPerson = b.yearOfBirth - (("yearOfDeath" in b) ? b.yearOfDeath : (new Date()).getFullYear());
+        return lastPerson > nextPerson ? 1 : -1;
+    });
+    console.log(oldest);
+    return oldest[0];
 };
 
 // Do not edit below this line
