73c58f553cbe4df3403fe510cd20feb39c317454
lab <lab@pop-os.fritz.box>
2024-12-21 19:14:43 +0100

    Finish 10_fibonacci

--- javascript-exercises/10_fibonacci/fibonacci.js
+++ javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,17 @@
-const fibonacci = function() {
+const fibonacci = function(x) {
+    let intX = parseInt(x)
+    
+    if (intX < 0) return 'OOPS';
 
+    fibArr = new Array(intX);
+    fibArr[0] = 1;
+    fibArr[1] = 1;
+    for (let i = 0; i < fibArr.length; i++) {
+        if (i > 1) {
+            fibArr[i] = fibArr[i-2] + fibArr[i-1];
+        }
+    }
+    return fibArr[x-1];
 };
 
 // Do not edit below this line
