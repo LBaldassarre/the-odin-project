d7fc0312123ff84d1331a779155677faeef7f06d
Lucas Baldassarre <lbaldassarre.dev@gmail.com>
2025-05-29 19:28:19 +0200

    Add basic cell handling with js

--- tic-tac-toe/script.js
+++ tic-tac-toe/script.js
@@ -6,3 +6,18 @@ const p2Name = 'Julia';
 const p2Icon = 'O';
 
 const testMatch = controller.createMatch(p1Name, p1Icon, p2Name, p2Icon);
+
+const gameBoardCells = document.querySelectorAll('.cell');
+
+let moveCount = 0;
+gameBoardCells.forEach(cell => {
+    cell.addEventListener('click', (e) => {
+        if (moveCount % 2 == 0) {
+            e.target.innerHTML = 'X';
+        } else {
+            e.target.innerHTML = 'O';
+        }
+
+        moveCount++;
+    });
+});
